645f4c69e888a7aa5f5e87e71e8dde9ec9408620
Roman Dodin <dodin.roman@gmail.com>
2021-08-12 11:25:50 +0200

    allowed controller to work with config maps

--- controllers/srlinux_controller.go
+++ controllers/srlinux_controller.go
@@ -82,6 +82,7 @@ type SrlinuxReconciler struct {
 //+kubebuilder:rbac:groups=kne.srlinux.dev,resources=srlinuxes/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=kne.srlinux.dev,resources=srlinuxes/finalizers,verbs=update
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
