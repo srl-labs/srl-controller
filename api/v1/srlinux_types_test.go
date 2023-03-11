da6ff95e0a363e52ca158395276e6610dc697c14
Roman Dodin <dodin.roman@gmail.com>
2023-03-11 13:57:20 +0100

    added tests for getModel and getConstraints funcs

--- api/v1/srlinux_types_test.go
+++ api/v1/srlinux_types_test.go
@@ -191,3 +191,77 @@ func TestInitVersion(t *testing.T) {
 		)
 	}
 }
+
+func TestGetConstraints(t *testing.T) {
+	tests := []struct {
+		desc string
+		spec *SrlinuxSpec
+		want map[string]string
+	}{
+		{
+			desc: "no constraints, default applies",
+			spec: &SrlinuxSpec{},
+			want: defaultConstraints,
+		},
+		{
+			desc: "constraints are present",
+			spec: &SrlinuxSpec{
+				Constraints: map[string]string{"cpu": "2", "memory": "4Gi"}},
+
+			want: map[string]string{"cpu": "2", "memory": "4Gi"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			img := tt.spec.GetConstraints()
+
+			if !cmp.Equal(img, tt.want) {
+				t.Fatalf(
+					"%s: actual and expected inputs do not match\nactual: %+v\nexpected:%+v",
+					tt.desc,
+					img,
+					tt.want,
+				)
+			}
+		},
+		)
+	}
+}
+
+func TestGetModel(t *testing.T) {
+	tests := []struct {
+		desc string
+		spec *SrlinuxSpec
+		want string
+	}{
+		{
+			desc: "no model specified, default applies",
+			spec: &SrlinuxSpec{},
+			want: defaultSrlinuxVariant,
+		},
+		{
+			desc: "model is present",
+			spec: &SrlinuxSpec{
+				Model: "ixr-10e",
+			},
+			want: "ixr-10e",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			img := tt.spec.GetModel()
+
+			if !cmp.Equal(img, tt.want) {
+				t.Fatalf(
+					"%s: actual and expected inputs do not match\nactual: %+v\nexpected:%+v",
+					tt.desc,
+					img,
+					tt.want,
+				)
+			}
+		},
+		)
+	}
+}
